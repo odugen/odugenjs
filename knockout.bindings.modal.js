b057834a972029b1c3eb342df6a180c0db7e7424
Odugen <odugen@gmail.com>
2012-11-06 17:42:07 +0400

    twitter-bootstrap modal knockoutjs binding

---
+++ knockout.bindings.modal.js
@@ -0,0 +1,25 @@
+﻿ko.bindingHandlers.modal = {
+   act: function (element, valueAccessor) {
+      var value = valueAccessor();
+      var binding = ko.utils.unwrapObservable(value);
+      binding ? $(element).modal('show') : $(element).modal('hide');
+   },
+   init: function (element, valueAccessor) {
+      $(element).on('hidden', function () {
+         var value = valueAccessor();
+         if (value()) {
+            value(false);
+         }
+      });
+      $(element).on('shown', function () {
+         var value = valueAccessor();
+         if (!value()) {
+            value(true);
+         }
+      });
+      ko.bindingHandlers.modal.act(element, valueAccessor);
+   },
+   update: function (element, valueAccessor) {
+      ko.bindingHandlers.modal.act(element, valueAccessor);
+   }
+};
