0d2841e543e38564de4daa26e10886d2263c50b3
odugen <odugen@gmail.com>
2012-11-13 13:19:52 +0400

    Update knockout.bindings.modal.js
    if static is defined disable keyboard events

--- knockout.bindings.modal.js
+++ knockout.bindings.modal.js
@@ -1,18 +1,38 @@
-﻿ko.bindingHandlers.modal = {
+ko.bindingHandlers.modal = {
+   getValue: function (valueAccessor) {
+      var value = valueAccessor();
+      var binding = ko.utils.unwrapObservable(value);
+      if (typeof binding === 'object') {
+         return binding.on;
+      }
+      return value;
+   },
    act: function (element, valueAccessor) {
       var value = valueAccessor();
       var binding = ko.utils.unwrapObservable(value);
-      binding ? $(element).modal('show') : $(element).modal('hide');
+      var $el = $(element);
+      var state = false;
+      if (typeof binding === 'object') {
+         state = binding.on() || false;
+         if (binding.static) {
+            $el.modal({ backdrop: 'static', keyboard: false });
+         }
+      }
+      if (typeof binding === 'boolean') {
+         state = binding;
+      }
+
+      state ? $el.modal('show') : $el.modal('hide');
    },
    init: function (element, valueAccessor) {
       $(element).on('hidden', function () {
-         var value = valueAccessor();
+         var value = ko.bindingHandlers.modal.getValue(valueAccessor);
          if (value()) {
             value(false);
          }
       });
       $(element).on('shown', function () {
-         var value = valueAccessor();
+         var value = ko.bindingHandlers.modal.getValue(valueAccessor);
          if (!value()) {
             value(true);
          }
