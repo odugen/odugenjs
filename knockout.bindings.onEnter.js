334f4f5d86ce08a465db43c1c482342f18beb66a
Odugen <odugen@gmail.com>
2012-11-13 13:25:46 +0400

    onEnter
    calls method onEnter

---
+++ knockout.bindings.onEnter.js
@@ -0,0 +1,14 @@
+﻿// thanks to: http://geekswithblogs.net/michelotti/archive/2011/10/11/knockoutjs-custom-binding-for-invoking-an-action-with-enter-key.aspx
+ko.bindingHandlers.onEnter = {
+   init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+      var allBindings = allBindingsAccessor();
+      $(element).keypress(function (event) {
+         var keyCode = (event.which ? event.which : event.keyCode);
+         if (keyCode === 13) {
+            allBindings.onEnter.call(viewModel);
+            return false;
+         }
+         return true;
+      });
+   }
+};
